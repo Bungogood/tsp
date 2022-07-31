4d59aff44f2675e64e97b9942bff5848515ec1b6
Bungogood <github@bungogood.com>
2022-07-31 21:10:20 +0100

    Adding MST

---
+++ src/com/bungogood/tsp/model/MST.java
@@ -0,0 +1,15 @@
+package com.bungogood.tsp.model;
+
+/**
+ * A Tree is a Graph with no Cycles
+ * A Minimum Spanning Tree is the Smallest tree which connects all vertices
+ */
+
+public class MST extends Graph {
+
+    public MST () {
+        super();
+    }
+
+    // Add static methods for mst checks
+}
